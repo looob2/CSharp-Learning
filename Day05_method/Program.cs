0f16d5b37945da28dca031d9d5a336f947b5c2f7
looob2 <1223883869@qq.com>
2026-07-07 23:41:44 +0800

    method learning

---
+++ Day05_method/Program.cs
@@ -0,0 +1,44 @@
+﻿int hour = int.Parse(Console.ReadLine());
+int minute = int.Parse(Console.ReadLine());
+int second = int.Parse(Console.ReadLine());
+while (hour > 0 || minute > 0 || second > 0)
+{
+    hour--;
+
+    if(hour < 0)
+    {
+        hour = 0;
+    }
+
+    if (second == 0)//当小时大于1时,分与秒两个为0的时候会无法通过while,故重置为60
+    {
+        second = 60;
+    }
+
+    if (minute == 0)
+    { 
+        minute = 60;
+    }
+
+    while (minute > 0 || second > 0)//分和秒只要同时为0时就会跳出
+    {
+        if (second == 0)
+        {
+            second = 60;
+        }
+
+        minute--;
+
+        if (minute < 0)
+        {
+            minute = 59;
+        }
+
+        while (second > 0)
+        {                      
+            second--;
+
+            Console.WriteLine($"{hour}:{minute}:{second}");
+        }
+    }   
+}
