72184384c873e6ba00eeb234b1f6ee3e5bcb3f41
looob2 <1223883869@qq.com>
2026-07-25 23:56:12 +0800

    Interface

---
+++ Day17_abstract/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Security;
+
+interface Iregister
+{
+    void register();
+}
+class Person : Iregister
+{
+    public void register()
+    {
+        Console.WriteLine("人已登记!");
+    }
+}
+class Car : Iregister
+{
+    public void register()
+    {
+        Console.WriteLine("车已登记!");
+    }
+}
+class Hourse : Iregister
+{
+    public void register()
+    {
+        Console.WriteLine("房已登记!");
+    }
+}
+
+interface Ifly
+{
+    void fly();
+}
+interface Iwalk
+{
+    void walk();
+}
+interface Iswim
+{
+    void swim();
+}
+class Animal : Ifly , Iswim , Iwalk
+{
+    public void walk() { }
+    public void fly() { }
+    public void swim() { }
+}
+class sparrow : Animal
+{
+    public void fly() { }
+    public void walk() { }
+}
+class swan : Animal
+{
+    public void fly() { }
+    public void walk() { }
+}
+class parrot : Animal
+{
+    public void fly() { }
+    public void walk() { }
+}
+class ostrich : Animal
+{
+    public void walk() { }
+}
+class Helicopter : Ifly
+{
+    public void fly() { }
+}
