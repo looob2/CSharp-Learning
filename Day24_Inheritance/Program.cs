b1c9daf4e072ad8ac52cd0484e49bc2ec1024e54
looob2 <1223883869@qq.com>
2026-07-31 23:13:17 +0800

    inheritance learning

---
+++ Day24_Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿class Worker
+{
+    public Worker(string duties , string type)
+    {
+        Duties = duties;
+        Type = type;
+    }
+
+    public string Duties {  get; set; }
+    public string Type {  get; set; }
+
+    public virtual void Work()
+    {
+        Console.WriteLine($"{Type}在做{Duties}");
+    }
+}
+class Programmer : Worker
+{
+    public Programmer(string duties , string type) : base(duties , type) { }
+}
+class Planner : Worker
+{
+    public Planner(string duties, string type) : base(duties , type) { }
+}
+class Artist : Worker
+{
+    public Artist(string duties, string type) : base (duties , type) { }
+}
+ 
