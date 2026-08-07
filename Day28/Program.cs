c9139d49730be9f668f978ce678b804f8db02722
looob2 <1223883869@qq.com>
2026-08-07 23:30:08 +0800

    generic learning

---
+++ Day28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+void judge<T>()
+{
+    if(typeof(T) == typeof(int)) 
+    {
+        Console.WriteLine($"整形,{Unsafe.SizeOf<T>()}字节");
+    }
+    else if (typeof(T) == typeof(char))
+    {
+        Console.WriteLine($"字符,{Unsafe.SizeOf<T>()}字节");
+    }
+    else if (typeof(T) == typeof(float))
+    {
+        Console.WriteLine($"浮点,{Unsafe.SizeOf<T>()}字节");
+    }
+    else if (typeof(T) == typeof(string))
+    {
+        Console.WriteLine($"字符串,{Unsafe.SizeOf<T>()}字节");
+    }
+    else
+    {
+        Console.WriteLine("其他类型");
+    }
+}
+
+judge<int>();
+judge<char>();
+judge<string>();
+judge<float>();
+judge<double>();
+
