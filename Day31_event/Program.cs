503e231a7e596db0969caef8e0cf38ff1e2b992c
looob2 <1223883869@qq.com>
2026-08-16 23:54:19 +0800

    event learning

---
+++ Day31_event/Program.cs
@@ -0,0 +1,45 @@
+﻿WaterHeater waterHeater = new();
+waterHeater.Use();
+
+class WaterHeater
+{
+    private int temperature;
+    public WaterHeater()
+    {
+        temperature = 21;
+    }
+    public void Heater()
+    {
+        temperature++;
+    }
+    public void Alarm()
+    {
+        if(temperature > 95)
+        {
+            Console.WriteLine($"水的温度:{temperature}");
+        }
+    }
+    public void Monitor()
+    {
+        if (temperature <= 95)
+        {
+            Console.WriteLine("水没烧开");
+        }
+        else if (temperature > 95)
+        {
+            Console.WriteLine("水烧开了!");
+        }
+    }
+
+    public event Action action;
+    public void Use()
+    {
+        action = Heater;
+        action += Alarm;
+        action += Monitor;
+        while (temperature < 100)
+        {
+            action();
+        }
+    }
+}
