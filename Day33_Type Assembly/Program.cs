4936a685fa46048379aba760f19c57f6cb44dcb1
looob2 <1223883869@qq.com>
2026-08-25 22:45:04 +0800

    Reflection

---
+++ Day33_Type Assembly/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+Assembly assembly = Assembly.LoadFrom("F:\\GameDeveloperLearning\\Player\\bin\\Debug\\Player.dll");
+Type? player = assembly.GetType("Player.Player");
+Activator.CreateInstance(player);
+ConstructorInfo[] constructors = player.GetConstructors();
+foreach (ConstructorInfo constructor in constructors)
+{
+    Console.WriteLine(constructor);
+}
---
+++ Player/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    public class Player
+    {
+        private string name;
+        private int hp;
+        private int attack;
+        private int defence;
+
+        public Player()
+        {
+            name = "Ghost";
+            hp = 10;
+            attack = 5;
+            defence = 5;
+        }
+        public Player(string name , int hp , int attack , int defence)
+        {
+            this.name = name;
+            this.hp = hp;
+            this.attack = attack;
+            this.defence = defence;
+        }
+    }
+}
