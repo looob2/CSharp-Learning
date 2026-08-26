43ba98024af865ff0948dd0d0eb0668467dcf750
looob2 <1223883869@qq.com>
2026-08-26 23:58:38 +0800

    Iterator

---
+++ Day34/Program.cs
@@ -0,0 +1,23 @@
+﻿MonsterManager monsterManager = new();
+foreach (string monster in monsterManager.GetMonsters())
+{
+    Console.WriteLine(monster);
+}
+
+class MonsterManager
+{
+    private List<string> monsters = new()
+    {
+        "史莱姆",
+        "僵尸",
+        "哥布林"
+    };
+
+    public IEnumerable<string> GetMonsters()
+    {
+        foreach (string monster in monsters)
+        {
+            yield return monster;
+        }
+    }
+}
